3a5294155ad79d047bd3773ffdc22e4b8e9610cb
Gleb Dolgoborodov <glebdolgoborodov@yandex.ru>
2024-07-13 12:34:35 +0300

    1

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Cube : MonoBehaviour
+{
+    private float _lowerLimit = 2f;
+    private float _upperLimit = 6f;
+
+    private Renderer _renderer;
+    private bool _hasChangedColor = false;
+    private bool _destroyTimerStarted = false;
+    private Spawner _spawner;
+
+    public void SetColor(Color color)
+    {
+        _renderer.material.color = color;
+    }
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Platform>() != null && !_hasChangedColor)
+        {
+            _hasChangedColor = true;
+            _renderer.material.color = Color.black;
+
+            if (!_destroyTimerStarted)
+            {
+                _destroyTimerStarted = true;
+                float lifeTime = Random.Range(_lowerLimit, _upperLimit);
+
+                StartCoroutine(DestroyAfterTime(lifeTime));
+            }
+        }
+    }
+
+    private IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _spawner.ReturnCubeToPool(this);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Cube _cubePrefab;
+
+    [SerializeField] private Transform _spawnArea;
+
+    [SerializeField] private int _poolSize;
+
+    [SerializeField] private float _spawnInterval = 2f;
+    [SerializeField] private float _minLifeTime = 2f;
+    [SerializeField] private float _maxLifeTime = 5f;
+
+    [SerializeField] private Color _cubeColor = Color.red;
+
+    private ObjectPool<Cube> _cubePool;
+
+    private float _spread = 15f;
+
+    public void ReturnCubeToPool(Cube cube)
+    {
+        _cubePool.Release(cube);
+    }
+
+    private void Awake()
+    {
+        _cubePool = new ObjectPool<Cube>(
+            () => Instantiate(_cubePrefab, _spawnArea.position, Quaternion.identity),
+            cube => cube.gameObject.SetActive(true),
+            cube => cube.gameObject.SetActive(false),
+            cube => Destroy(cube.gameObject),
+            false,
+            _poolSize
+        );           
+    }
+
+    private void Update()
+    {
+        if (Time.time >= _spawnInterval)
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Cube cube = _cubePool.Get();
+
+        cube.transform.position = new Vector3(Random.Range(_spawnArea.position.x - _spread, _spawnArea.position.x + _spread),
+                                                           _spawnArea.position.y,
+                                              Random.Range(_spawnArea.position.z - _spread, _spawnArea.position.z + _spread));
+
+        cube.SetColor(_cubeColor);
+    }
+}
